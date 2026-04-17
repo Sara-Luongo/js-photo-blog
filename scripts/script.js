7ba4a25440741d2007a3578d6c1e5b7f570bfda4
sara luongo <saraluongo202002@gmail.com>
2026-04-17 12:40:00 +0200

    effettuata chiamata all'API e fatto controllo in caso di  errore nella catch

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 'use strict';
+const ApiUrl = 'https://lanciweb.github.io/demo/api/pictures/';
+
+fetch(ApiUrl)
+    .then(response => {
+        return response.json()
+    })
+    .then(dataJson => {
+        console.log(dataJson);
+    })
+    .catch(error => {
+        console.error(error);
+
+    })
+
