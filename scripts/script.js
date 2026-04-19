4a41fe1330be2f60cebd9a8ffd364bfc5e2b1d72
sara luongo <saraluongo202002@gmail.com>
2026-04-19 12:49:01 +0200

    impostata struttura html, creata card stilizzata con css, fatti anche test per la responsiv

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
 'use strict';
+//variabile contenente l'url
 const ApiUrl = 'https://lanciweb.github.io/demo/api/pictures/';
+//variabili che prendono elementi dall'html per i messaggi. 
 const elemCaricamento = document.querySelector('#mess-caricamento');
 const elemErrCaricamento = document.querySelector('#mess-errore-caricamento');
 
+//inner html che fa apparire a schermo il mess di caricamento.
 elemCaricamento.innerHTML = 'Caricamento';
 
-
+//chiamata API.
 fetch(ApiUrl)
     .then(response => {
         return response.json()
@@ -18,6 +21,7 @@ fetch(ApiUrl)
         elemErrCaricamento.innerHTML = 'errore durante il caricamento';
 
     })
+    //svuota la stringa di caricamento 
     .finally(() => {
         elemCaricamento.innerHTML = '';
     })
