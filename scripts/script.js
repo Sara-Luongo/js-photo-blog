1dc5398515f748da48915715534c6c52486292df
sara luongo <saraluongo202002@gmail.com>
2026-04-20 17:48:08 +0200

    modificato il js aggiunti event-listeners per far apparire il modale al click

--- scripts/script.js
+++ scripts/script.js
@@ -7,24 +7,25 @@ const elemCaricamento = document.querySelector('#mess-caricamento');
 const elemErrCaricamento = document.querySelector('#mess-errore-caricamento');
 //variabile che prende contenitore card in html.
 const galleryContainer = document.querySelector('#gallery-container');
-//riferimenti html per il modale 
+
+//riferimenti html per il modale. 
 const myModalContainer = document.querySelector('#my-modal');
 const imgModal = document.querySelector('#img-full');
 const buttonModal = document.querySelector('#modal-button');
-
-
 //inner html che fa apparire a schermo il mess di caricamento.
 elemCaricamento.innerHTML = 'Caricamento';
-
-
 
 // funzione che genera card nella gallery
 function generatoreCards(gallery) {
     let galleryHtml = '';
+    let contenitoreCard = '';
 
     for (const dataGallery of gallery) {
-        galleryHtml += `  <div class=" col-md-6 col-lg-4 ">
-                    <div class="my-card mx-auto">
+        contenitoreCard = document.createElement('div');
+        contenitoreCard.classList.add('col-md-6');
+        contenitoreCard.classList.add('col-lg-4');
+
+        galleryHtml = `   <div class="my-card mx-auto">
                         <div class="wrapper-img">
                             <img src="${dataGallery.url}" class="grandezza-img" alt="immagine gallery">
                             <img src="./img/pin.svg" class="img-absolute" alt="pin">
@@ -33,11 +34,20 @@ function generatoreCards(gallery) {
                             <p>${dataGallery.title}</p>
                             <p>${dataGallery.date}</p>
                         </div>
-                    </div>
-                </div>
-        `
+                    </div>`
+
+        contenitoreCard.innerHTML = galleryHtml;
+
+        contenitoreCard.addEventListener("click", function () {
+            myModalContainer.classList.remove('none-d');
+            imgModal.src = dataGallery.url
+
+        });
+        galleryContainer.appendChild(contenitoreCard);
     }
-    galleryContainer.innerHTML = galleryHtml;
+    buttonModal.addEventListener('click', function () {
+        myModalContainer.classList.add('none-d');
+    })
 };
 
 
